6a04e18f32a8bde18e87ef9a7f7c283bca9512bf
dungnq271 <dungnguyen2712002@gmail.com>
2024-01-09 16:56:50 +0700

    Remove minor bug

--- inference.sh
+++ inference.sh
@@ -4,7 +4,7 @@ python detect_crop_image_save_bbox.py \
        --conf 0.25 \
        --iou-thres 0.5 \
        --img-size 640 \
-       --source "public_test/images" \       
+       --source "public_test/images" \
        --project "yolov7_face_test_20" \
        --name  "annotated_imgs" \
        --cropped-dir "cropped_face_imgs" \
