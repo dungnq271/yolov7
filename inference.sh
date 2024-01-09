38469c3d19d20ef5895c63a7d929df380d958d98
dungnq271 <dungnguyen2712002@gmail.com>
2024-01-09 16:18:03 +0700

    Add new inference file for face-analysis

---
+++ inference.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+python detect_crop_image_save_bbox.py \
+       --weights checkpoints/best.pt \
+       --conf 0.25 \
+       --iou-thres 0.5 \
+       --img-size 640 \
+       --source "public_test/images/100147591.jpg" \
+       --project "yolov7_face_test_20" \
+       --name  "annotated_imgs" \
+       --cropped-dir "cropped_face_imgs" \
+       --no-trace \
+       --exist-ok
+       
