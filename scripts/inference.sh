09b7b41dee7ef38032497d149c10a6a25a91cf4c
dungnq271 <dungnguyen2712002@gmail.com>
2024-01-19 21:59:10 +0700

    Add script to detect and crop images

---
+++ scripts/inference.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+python detect_crop_image_save_bbox.py \
+       --weights "/kaggle/input/face-analysis-hackathon-public-test-20/best.pt" \
+       --conf 0.25 \
+       --iou-thres 0.5 \
+       --img-size 640 \
+       --source "/kaggle/input/face-analysis-hackathon-public-test-20/public_test/public_test" \
+       --project "../yolov7_face_test_20" \
+       --name  "annotated_imgs" \
+       --cropped-dir "cropped_face_imgs" \
+       --no-trace \
+       --exist-ok \
+       --nosave
