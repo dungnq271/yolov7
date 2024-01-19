f31e0979390efa5ba45586777af7786bb7074056
dungnq271 <dungnguyen2712002@gmail.com>
2024-01-20 00:21:21 +0700

    Update source and project_name

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 python detect_crop_image_save_bbox.py \
-       --weights "/kaggle/input/face-analysis-hackathon-public-test-20/best.pt" \
+       --weights "./weights/yolov7-e6e.pt" \
        --conf 0.25 \
        --iou-thres 0.5 \
        --img-size 640 \
-       --source "/kaggle/input/face-analysis-hackathon-public-test-20/public_test/public_test" \
-       --project "../yolov7_face_test_20" \
+       --source "./images" \
+       --project "./yolov7_face_test_20" \
        --name  "annotated_imgs" \
        --cropped-dir "cropped_face_imgs" \
        --no-trace \
