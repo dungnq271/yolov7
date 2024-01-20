4a7db4c291c9bb163c6850d62533184fd3509950
dungnq271 <dungnguyen2712002@gmail.com>
2024-01-20 11:32:34 +0700

    Delete inference_v2

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 python detect_crop_image_save_bbox.py \
-       --weights "./weights/yolov7-e6e.pt" \
+       --weights "./checkpoints/weights/best.pt" \
        --conf 0.25 \
        --iou-thres 0.5 \
        --img-size 640 \
-       --source "./images" \
-       --project "./yolov7_face_test_20" \
+       --source "./public_test" \
+       --project "yolov7_face_test_20" \
        --name  "annotated_imgs" \
        --cropped-dir "cropped_face_imgs" \
        --no-trace \
